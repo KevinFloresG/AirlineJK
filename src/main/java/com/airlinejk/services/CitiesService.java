c3d2af9aa549ddf8a19a1b0603239342c2730afc
Kevin Flores Garcia <kevinfg1413@gmail.com>
2021-04-07 10:49:13 -0600

    CitiesService Terminado

--- src/main/java/com/airlinejk/services/CitiesService.java
+++ src/main/java/com/airlinejk/services/CitiesService.java
@@ -3,6 +3,7 @@ package com.airlinejk.services;
 import com.airlinejk.business_logic.Cities;
 import com.airlinejk.daos.CitiesDao;
 import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -17,16 +18,51 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CitiesService extends HttpServlet {
     
-    private CitiesDao dao = new CitiesDao();
-    private Gson gson = new Gson();
+    private final CitiesDao dao = new CitiesDao();
+    private final Gson gson = new Gson();
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+        
         switch(request.getServletPath()){
+            case "/cities/get" : get(request, response); break;
+            case "/cities/add" : insert(request, response); break;
+            case "/cities/update" : update(request, response); break;
             case "/cities/all" : getAll(response); break;
+            case "/cities/delete" : delete(request, response); break;
         }   
+    }
+    
+    private void insert(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        BufferedReader reader = request.getReader();
+        Cities city = gson.fromJson(reader, Cities.class);
+        dao.insert(city);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+    
+    private void update(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        BufferedReader reader = request.getReader();
+        Cities city = gson.fromJson(reader, Cities.class);
+        dao.update(city);
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+    
+    private void get(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String id = request.getParameter("id");
+        Cities city = dao.get(id);
+        String allJson = gson.toJson(city);
+        PrintWriter out = response.getWriter();
+        out.print(allJson);
+        out.flush();
+    }
+    
+    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        String id = request.getParameter("id");
+        dao.delete(id);
+        response.setStatus(HttpServletResponse.SC_OK);
     }
 
     private void getAll(HttpServletResponse response) throws IOException{
