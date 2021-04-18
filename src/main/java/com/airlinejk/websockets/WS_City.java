683f6de22d4730af18bec3b9c2b315fae3bcef2d
Java64x <55204353+Java64x@users.noreply.github.com>
2021-04-18 14:15:58 -0600

    WS de Cities

--- src/main/java/com/airlinejk/websockets/WS_City.java
+++ src/main/java/com/airlinejk/websockets/WS_City.java
@@ -1,7 +1,7 @@
 package com.airlinejk.websockets;
 
-import com.airlinejk.business_logic.Countries;
-import com.airlinejk.daos.CountriesDao;
+import com.airlinejk.business_logic.Cities;
+import com.airlinejk.daos.CitiesDao;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
@@ -22,20 +22,20 @@ import javax.websocket.server.ServerEndpoint;
  * @author Kevin Flores, Javier Amador
  */
 @ServerEndpoint(
-    value = "/country",
+    value = "/city",
     decoders = {Decodifier.class},
     encoders = {Encodifier.class}
 )
-public class WS_Country {
+public class WS_City {
 
-    private final CountriesDao dao = new CountriesDao();
+    private final CitiesDao dao = new CitiesDao();
     private final Gson gson = new Gson();
     private final String JSON = "{\"type\":\"%s\", \"content\":%s}";
     
     @OnOpen
     public void onOpen(Session s) throws IOException, EncodeException {
-        List<Countries> l = dao.all();
-        String json = gson.toJson(l, new TypeToken<List<Countries>>(){}.getType());
+        List<Cities> l = dao.all();
+        String json = gson.toJson(l, new TypeToken<List<Cities>>(){}.getType());
         String finalJson = String.format(JSON, "all", json);
         s.getBasicRemote().sendText(finalJson);
         /*
@@ -52,34 +52,34 @@ public class WS_Country {
             throws IOException, EncodeException {
         switch(request.getString("type")){
             case "delete":
-                deleteCountry(s, request.getString("content"));
+                deleteCity(s, request.getString("content"));
                 break;
             case "add":
-                addCountry(s, request.getString("content"));
+                addCity(s, request.getString("content"));
                 break;
             case "update":
-                updateCountry(s, request.getString("content"));
+                updateCity(s, request.getString("content"));
                 break;
         }
     }
     
-    private void updateCountry(Session s, String country) throws IOException{
-        Countries f = gson.fromJson(country, Countries.class);
+    private void updateCity(Session s, String country) throws IOException{
+        Cities f = gson.fromJson(country, Cities.class);
         dao.update(f);
         f = dao.get(f.getId());
-        broadcast(s, "update", gson.toJson(f, Countries.class));
+        broadcast(s, "update", gson.toJson(f, Cities.class));
     }
     
-    private void deleteCountry(Session s, String country) throws IOException{
-        Countries f = gson.fromJson(country, Countries.class);
+    private void deleteCity(Session s, String country) throws IOException{
+        Cities f = gson.fromJson(country, Cities.class);
         dao.delete(f.getId());
         broadcast(s, "delete",country);
     }
     
-    private void addCountry(Session s, String country) throws IOException{
-        Countries f = gson.fromJson(country, Countries.class);
+    private void addCity(Session s, String city) throws IOException{
+        Cities f = gson.fromJson(city, Cities.class);
         dao.insert(f);
-        broadcast(s, "add", gson.toJson(f, Countries.class));
+        broadcast(s, "add", gson.toJson(f, Cities.class));
     }
     
     private void broadcast(Session s, String type, String content) throws IOException{
@@ -90,8 +90,8 @@ public class WS_Country {
         }
     }
 
-    public JsonObject listAsJsonObj(List<Countries> l){
-        String json = gson.toJson(l, new TypeToken<List<Countries>>(){}.getType());
+    public JsonObject listAsJsonObj(List<Cities> l){
+        String json = gson.toJson(l, new TypeToken<List<Cities>>(){}.getType());
         String j2 = "{ \"content\" : "+json+"}";
         System.out.println(j2);
         JsonReader jsonReader = Json.createReader(new StringReader(j2));
